4e9fe4a42595355cf8c0bc54d9483284aae5842e
anilmawji <mawjianil1@gmail.com>
2022-11-21 00:28:05 -0700

    add login/signup links

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,21 +8,33 @@ const Navbar = () => {
         <img className="nav-logo" src="" alt="logo" />
       </div>
       <div className="nav-left">
-        <div className="nav-links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/">Products</Link>
-            </li>
-            <li>
-              <Link to="/">Recommendations</Link>
-            </li>
-            <li>
-              <Link to="/">Wishlist</Link>
-            </li>
-          </ul>
+        <div className="link-container">
+          <div className="page-links">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Products</Link>
+              </li>
+              <li>
+                <Link to="/">Recommendations</Link>
+              </li>
+              <li>
+                <Link to="/">Wishlist</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="user-links">
+            <ul>
+              <li>
+                <Link to="/">Log in</Link>
+              </li>
+              <li>
+                <Link to="/">Sign up</Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <SearchBar placeholder="Search books by title or author..." />
       </div>
@@ -31,32 +43,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// const Navbar = () => {
-//   return (
-//     <nav>
-//       <div className="nav-links">
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/">Products</Link>
-//           </li>
-//           <li>
-//             <Link to="/">Recommendations</Link>
-//           </li>
-//           <li>
-//             <Link to="/">Wishlist</Link>
-//           </li>
-//         </ul>
-//       </div>
-//       <div className="nav-bottom">
-//         <div className="logo-container">
-//           <img className="nav-logo" src="" alt="logo" />
-//         </div>
-//         <SearchBar placeholder="Search books by title or author..." />
-//       </div>
-//     </nav>
-//   );
-// };
