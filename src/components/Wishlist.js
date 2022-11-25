86093df0f82679c9c3638cf01860101f1e63a700
brian-vo <brian.vo1@ucalgary.ca>
2022-11-24 19:44:44 -0700

    move pages into 'components' folder for clarity

---
+++ src/components/Wishlist.js
@@ -0,0 +1,12 @@
+import Banner from "../Banner";
+
+const Wishlist = () => {
+  return (
+    <div className="wishlist">
+      {" "}
+      <Banner title="Wishlist"></Banner>
+    </div>
+  );
+};
+
+export default Wishlist;
