5450261b74b946e5cace3a5dea2984e117c83c92
brian-vo <brian.vo1@ucalgary.ca>
2022-11-26 02:12:04 -0700

    add wishlist creation function, fix query logic

--- api/users.py
+++ api/users.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, jsonify, request
 from . import db
 from .models import Book
+
 from .models import Shopping_Cart
 from .models import Derived_From
 from .models import Stores
+
+from .models import Wishlist
+from .models import Includes
 from .models import User
 
 from sqlalchemy.sql import text
@@ -75,17 +79,17 @@ def add_cart(user_id):
         db.session.add(new_cart)
         db.session.commit()
 
-        add_item(user_id)
+        add_item_cart(user_id)
         return 'Added', 200
     else:
-        add_item(user_id)
+        add_item_cart(user_id)
         return 'Added', 200
 
 # adds item to stores relationship relative to cart
-def add_item(user_id):
+def add_item_cart(user_id):
     cart_data = request.get_json()
 
-    cart = db.session.query(Shopping_Cart).filter(user_id == user_id).first()
+    cart = db.session.query(Shopping_Cart).filter(Shopping_Cart.user_id == user_id).one()
     new_stores = Stores(cart_id = cart.cart_id, isbn = cart_data['isbn'] )
     db.session.add(new_stores)
     db.session.commit()
@@ -98,7 +102,7 @@ def cart_data(user_id):
     cart_exists = db.session.query(db.exists().where(Shopping_Cart.user_id == user_id)).scalar()
 
     if cart_exists:
-        cart = db.session.query(Shopping_Cart).filter(user_id == user_id).first()
+        cart = db.session.query(Shopping_Cart).filter(Shopping_Cart.user_id == user_id).one()
         sql = text("FROM shopping_cart AS c, stores AS s, book AS b WHERE c.cart_id = :cart AND :cart= s.cart_id AND s.isbn = b.isbn GROUP BY b.isbn")
         cart_list = db.session.execute(sql, cart.cart_id, cart.cart_id)
 
@@ -113,7 +117,7 @@ def cart_delete_item(user_id):
 
     if cart_exists:
         cart_data = request.get_json()
-        cart = db.session.query(Shopping_Cart).filter(user_id == user_id).first()
+        cart = db.session.query(Shopping_Cart).filter(Shopping_Cart.user_id == user_id).one()
         db.session.query(Stores).filter_by(cart_id = cart.cart_id, isbn = cart_data['isbn']).delete()
         db.session.commit()
         
@@ -128,14 +132,14 @@ def cart_delete(user_id):
 
 
     if cart_exists:
-        cart = db.session.query(Shopping_Cart).filter(user_id == user_id).first()
+        cart = db.session.query(Shopping_Cart).filter(Shopping_Cart.user_id == user_id).one()
         derived_exists = db.session.query(db.exists().where(Derived_From.cart_id == cart.user_id)).scalar()
         
         if derived_exists:
             db.session.query(Derived_From).filter_by(cart_id = cart.cart_id).delete()
-        
-        db.session.query(Stores).filter_by(cart_id = cart.cart_id).delete()
-        db.session.query(Shopping_Cart).filter(user_id == user_id).delete()
+
+        db.session.query(Stores).filter_by(Stores.cart_id = cart.cart_id).delete()
+        db.session.query(Shopping_Cart).filter(Shopping_Cart.user_id == user_id).delete()
         db.session.commit()
         
         return 'DELETED', 200
@@ -143,5 +147,33 @@ def cart_delete(user_id):
         return 'NO CART', 404
 
 # ===========================================================
-# book_order FUNCTIONS
+# wishhlist FUNCTIONS
 # ===========================================================
+
+# add an item to shopping_cart - NOT SURE HOW TO HANDLE USER_ID yet
+@main.route('/wishlist/add/<user_id>', methods=['POST'])
+# checks if cart exists for that user, and creates if does not 
+def add_wishlist(user_id):
+    wishlist_exists = db.session.query(db.exists().where(Wishlist.user_id == user_id)).scalar()
+
+    if not wishlist_exists:
+        new_wishlist = Wishlist(user_id= user_id)
+        db.session.add(new_wishlist)
+        db.session.commit()
+
+        add_item_wishlist(user_id)
+        return 'Added', 200
+    else:
+        add_item_wishlist(user_id)
+        return 'Added', 200
+
+# adds item to stores relationship relative to cart
+def add_item_wishlist(user_id):
+    wishlist_data = request.get_json()
+
+    wishlist = db.session.query(Wishlist).filter(Wishlist.user_id == user_id).one()
+    new_stores = Includes(wishlist_id = wishlist.wishlist_id, isbn = wishlist_data['isbn'] )
+    db.session.add(new_stores)
+    db.session.commit()
+
+    return ''
