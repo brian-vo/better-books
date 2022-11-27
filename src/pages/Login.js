5ca2526007923cd4d200ddb86d3d564799bfc708
anilmawji <mawjianil1@gmail.com>
2022-11-27 02:13:05 -0700

    separate login and signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,110 +1,40 @@
-import React, { useState } from "react";
-import {
-  //MDBContainer,
-  MDBTabs,
-  MDBTabsItem,
-  MDBTabsLink,
-  MDBTabsContent,
-  MDBTabsPane,
-  MDBBtn,
-  //MDBIcon,
-  MDBInput,
-  MDBCheckbox,
-} from "mdb-react-ui-kit";
+import { MDBBtn, MDBInput, MDBCheckbox } from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
 
 const LogIn = () => {
-  const [justifyActive, setJustifyActive] = useState("tab1");
-
-  const handleJustifyClick = (value) => {
-    if (value === justifyActive) {
-      return;
-    }
-    setJustifyActive(value);
-  };
-
   return (
     <div className="log-in">
-      <MDBTabs
-        pills
-        justify
-        className="mb-3 d-flex flex-row justify-content-between"
-      >
-        <MDBTabsItem>
-          <MDBTabsLink
-            onClick={() => handleJustifyClick("tab1")}
-            active={justifyActive === "tab1"}
-          >
-            Login
-          </MDBTabsLink>
-        </MDBTabsItem>
-        <MDBTabsItem>
-          <MDBTabsLink
-            onClick={() => handleJustifyClick("tab2")}
-            active={justifyActive === "tab2"}
-          >
-            Register
-          </MDBTabsLink>
-        </MDBTabsItem>
-      </MDBTabs>
+      <Banner title="Login"></Banner>
+      <div className="login-container">
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Email address"
+          id="form1"
+          type="email"
+        />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="form2"
+          type="password"
+        />
 
-      <MDBTabsContent>
-        <MDBTabsPane show={justifyActive === "tab1"}>
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Email address"
-            id="form1"
-            type="email"
+        <div className="d-flex justify-content-between mx-4 mb-4">
+          <MDBCheckbox
+            name="flexCheck"
+            value=""
+            id="flexCheckDefault"
+            label="Remember me"
           />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Password"
-            id="form2"
-            type="password"
-          />
+          <a href="!#">Forgot password?</a>
+        </div>
 
-          <div className="d-flex justify-content-between mx-4 mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              value=""
-              id="flexCheckDefault"
-              label="Remember me"
-            />
-            <a href="!#">Forgot password?</a>
-          </div>
-
-          <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
-          <p className="text-center">
-            Not a member? <a href="#!">Register</a>
-          </p>
-        </MDBTabsPane>
-
-        <MDBTabsPane show={justifyActive === "tab2"}>
-          <MDBInput wrapperClass="mb-4" label="Name" id="form1" type="text" />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Username"
-            id="form1"
-            type="text"
-          />
-          <MDBInput wrapperClass="mb-4" label="Email" id="form1" type="email" />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Password"
-            id="form1"
-            type="password"
-          />
-
-          <div className="d-flex justify-content-center mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              id="flexCheckDefault"
-              label="I have read and agree to the terms"
-            />
-          </div>
-
-          <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
-        </MDBTabsPane>
-      </MDBTabsContent>
+        <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
+        <p className="text-center register-link">
+          Not a member? <Link to="/signup">Register</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,31 @@
+import { MDBBtn, MDBInput, MDBCheckbox } from "mdb-react-ui-kit";
+import Banner from "../components/Banner";
+
 const SignUp = () => {
   return (
-    <div className="sign-up">
-      <h1>Sign up</h1>
+    <div className="signup">
+      <Banner title="Sign up"></Banner>
+      <div className="signup-container">
+        <MDBInput wrapperClass="mb-4" label="Name" id="form1" type="text" />
+        <MDBInput wrapperClass="mb-4" label="Username" id="form1" type="text" />
+        <MDBInput wrapperClass="mb-4" label="Email" id="form1" type="email" />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="form1"
+          type="password"
+        />
+
+        <div className="d-flex justify-content-center mb-4">
+          <MDBCheckbox
+            name="flexCheck"
+            id="flexCheckDefault"
+            label="I have read and agree to the terms"
+          />
+        </div>
+
+        <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
+      </div>
     </div>
   );
 };
