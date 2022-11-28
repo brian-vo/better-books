52e3e2114759f4979d1d2c5cd52148bea1fbae9c
anilmawji <mawjianil1@gmail.com>
2022-11-28 03:46:49 -0700

    update sign up

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,25 +2,33 @@ import { MDBBtn, MDBInput, MDBCheckbox } from "mdb-react-ui-kit";
 
 const SignUp = () => {
   return (
-    <div className="signup">
-      <div className="signup-container">
-        <MDBInput wrapperClass="mb-4" label="Name" id="form1" type="text" />
-        <MDBInput wrapperClass="mb-4" label="Username" id="form1" type="text" />
-        <MDBInput wrapperClass="mb-4" label="Email" id="form1" type="email" />
-        <MDBInput
-          wrapperClass="mb-4"
-          label="Password"
-          id="form1"
-          type="password"
-        />
-        <div className="d-flex justify-content-center mb-4">
-          <MDBCheckbox
-            name="flexCheck"
-            id="flexCheckDefault"
-            label="I have read and agree to the terms"
+    <div className="login-container">
+      <div className="login-form">
+        <div className="login-items">
+          <h1>Create an account</h1>
+          <MDBInput wrapperClass="mb-4" label="Name" id="form1" type="text" />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Username"
+            id="form1"
+            type="text"
           />
+          <MDBInput wrapperClass="mb-4" label="Email" id="form1" type="email" />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form1"
+            type="password"
+          />
+          <div className="d-flex justify-content-center mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              id="flexCheckDefault"
+              label="I have read and agree to the terms"
+            />
+          </div>
+          <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
         </div>
-        <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
       </div>
     </div>
   );
