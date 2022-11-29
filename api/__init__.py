009402553a3a4f1ae246800f5b1a5fb8d7dd2217
brian-vo <brian.vo1@ucalgary.ca>
2022-11-28 17:34:23 -0700

    remove unused imports

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-migrate = Migrate()
-ma = Marshmallow()
 
 def create_app():
         app = Flask(__name__)
@@ -15,8 +11,6 @@ def create_app():
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
         db.init_app(app)
-        migrate.init_app(app, db)
-        ma.init_app(app)
         
         from .users import main
         app.register_blueprint(main)
