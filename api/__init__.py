8aad65cca436c188b78c7de2eb580cb42eff1a3c
brian-vo <brian.vo1@ucalgary.ca>
2022-11-29 10:50:51 -0700

    fix login

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -10,8 +11,23 @@ def create_app():
         app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kQ4xei6b^@localhost:3306/bookshopdb'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['SECRET_KEY'] = 'q3t6v9y$B&E)H@McQfTjWnZr4u7x!z%C'
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
         db.init_app(app)
+
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login'
+        login_manager.init_app(app)
+
+        from .models import User
+
+        @login_manager.user_loader
+        def load_user(user_id):
+            u = (db.session.query(User).filter(User.user_id == user_id).one())
+            return u
         
         from .users import main
         app.register_blueprint(main)
