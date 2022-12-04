d575b00ba0e969b9e032f3297491bf1b37e71454
brian-vo <brian.vo1@ucalgary.ca>
2022-12-03 18:56:48 -0700

    display homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
-import FeaturedBook from "../components/FeaturedBook";
-import example_cover from "../img/example-cover.jpg";
 
 const Home = () => {
+  const [books, setBooks] = useState([]); 
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("/book/all_data");
+
+        const data = await response.json();
+
+        setBooks(data.books);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <div className="home">
       <Banner
@@ -12,16 +29,21 @@ const Home = () => {
       <div className="content-container">
         <h1 className="home-title">Featured Books</h1>
         <ul className="featured">
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
-          <FeaturedBook isbn="0" />
+          {books.map((book) => (
+            <li className="book-icon">
+              <Link to={"/book?isbn=" + book.isbn}>
+                <div className="icon-container">
+                  <img src={book.image_location} alt="book" />
+                </div>
+                <div className="icon-info">
+                  <div className="icon-title">{book.title}</div>
+                  <div className="icon-authors">
+                    {book.authors.map((author) => `${author.fname} ${author.lname}`).join(", ")}
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
