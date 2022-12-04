c7878690f2ab672428ce5892ef878fe8ec793af9
brian-vo <brian.vo1@ucalgary.ca>
2022-12-03 20:50:23 -0700

    load product page, atc not working yet

--- src/pages/BookPurchase.js
+++ src/pages/BookPurchase.js
@@ -1,50 +1,88 @@
-import example_cover from "../img/example-cover.jpg";
+import React, { useState, useEffect } from "react";
 import Review from "../components/Review";
+import { useLocation } from "react-router-dom";
 
-const BookPurchase = ({ isbn }) => {
-  return (
-    <div className="purchase-container">
-      <div className="purchase-content">
-        <div className="book-container">
-          <img src={example_cover} alt="book" />
-        </div>
-        <div className="purchase-info">
-          <h1>Book title</h1>
-          <p>
-            <h2>$24.99</h2>
-            <strong>Description:</strong>
-            <br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
-            repudiandae enim harum aliquam quam praesentium quae fuga magni
-            iure, ea dicta possimus dolorem tenetur autem veritatis! Nam nulla
-            mollitia similique? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Alias sed fuga ducimus beatae pariatur velit
-            deleniti odio eos, autem nisi molestiae illum neque minus, culpa
-            sapiente reprehenderit commodi dolor. Sunt? Lorem ipsum, dolor sit
-            amet consectetur adipisicing elit. Quisquam repudiandae enim harum
-            aliquam quam praesentium quae fuga magni iure, ea dicta possimus
-            dolorem tenetur autem veritatis! Nam nulla mollitia similique? Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Alias sed fuga
-            ducimus beatae pariatur velit deleniti odio eos, autem nisi
-            molestiae illum neque minus, culpa sapiente reprehenderit commodi
-            dolor. Sunt?
-          </p>
-          <div className="purchase-btns">
-            <button className="button">Add to Wishlist</button>
-            <button className="button">Leave Review</button>
-          </div>
-          <button className="button">Add to Cart</button>
-        </div>
+const BookPurchase = () => {
+  const [book, setBook] = useState({}); // Store the book in state
+
+  // Get the URL query string
+  const queryString = useLocation().search;
+
+  // Parse the query string to get the `isbn` value
+  const params = new URLSearchParams(queryString);
+  const isbn = params.get("isbn");
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      try {
+        // Make a request to the '/book/<book_isbn>/data' endpoint
+        const response = await fetch(`/book/${isbn}/data`);
+
+        // Parse the response as JSON
+        const data = await response.json();
+
+        // Update the book state with the returned data
+        setBook(data.books[0]);
+      } catch (error) {
+        // Handle any errors
+        console.error(error);
+      }
+    };
+
+    // Fetch the book when the component mounts
+    fetchBook();
+  }, [isbn]);
+
+  const handleAddToCart = async () => {
+    try {
+      // Make a request to the '/shopping_cart/add/<user_id>' endpoint
+      const response = await fetch(`/shopping_cart/add/`, {
+        method: 'POST',
+        body: JSON.stringify({
+          "isbn": "1507084220223",
+        }),
+      });
+      // Parse the response as JSON
+      const data = await response.json();
+
+      // Handle the response data
+      console.log(data);
+    } catch (error) {
+      // Handle any errors
+      console.error(error);
+    }
+  };
+
+return (
+  <div className="purchase-container">
+    <div className="purchase-content">
+      <div className="book-container">
+        <img src={book.image_location} alt="book" />
       </div>
-      <div className="review-content">
-        <h1>Reviews</h1>
-        <Review review_id="0"></Review>
-        <Review review_id="0"></Review>
-        <Review review_id="0"></Review>
-        <Review review_id="0"></Review>
+      <div className="purchase-info">
+        <h1>{book.title}</h1>
+        <p>
+          <h2>${book.price}</h2>
+          <strong>Description:</strong>
+          <br />
+          {book.description}
+        </p>
+        <div className="purchase-btns">
+          <button className="button">Add to Wishlist</button>
+          <button className="button">Leave Review</button>
+        </div>
+        <button className="button" onClick={handleAddToCart}>Add to Cart</button>
       </div>
     </div>
-  );
+    <div className="review-content">
+      <h1>Reviews</h1>
+      <Review review_id="0"></Review>
+      <Review review_id="0"></Review>
+      <Review review_id="0"></Review>
+      <Review review_id="0"></Review>
+    </div>
+  </div>
+);
 };
 
 export default BookPurchase;
