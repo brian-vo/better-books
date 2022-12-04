7194ff481621097f9134e2cc02fb34ebcf53bb44
brian-vo <brian.vo1@ucalgary.ca>
2022-12-04 10:57:18 -0700

    account page display all orders (should change to order history)

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,34 +1,32 @@
-import React, {useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import OrderItem from "../components/OrderItem";
 import "./accountnav.css";
-
-const SideNav = () => {
-  return (
-    <div className="sidenav">
-      <Link to="/account">Order History</Link>
-      <Link to="/wishlist">Wishlist</Link>
-      <Link to="/recommendations">Recommendations</Link>
-    </div>
-  );
-};
+import SideNav from "../components/SideNav";
 
 const Account = () => {
   const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     if (!token) {
       navigate("/login");
+      return;
     }
-  }, []);
 
-  const handleLogout = () => {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      // Send the HTTP request to load the orders data.
+  fetch(`/orders/all`, {
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  })
+    .then(response => response.json())
+    .then(response => setOrders(response.orders));
+}, []);
 
-    navigate("/");
-  };
+
+
 
   return (
     <div className="wishlist">
@@ -36,22 +34,32 @@ const Account = () => {
       <div className="wishlist-container">
         <h1>Order History</h1>
         <table className="order-table">
-          <thead>
-            <tr className="order-header">
-              <td>Order#</td>
-              <td>Quantity</td>
-              <td>Total</td>
-              <td>Status</td>
-              <td>Prepared date</td>
-              <td>Shipped date</td>
-              <td>Delivered date</td>
-            </tr>
-            <OrderItem order_id="000000000000"></OrderItem>
-            <OrderItem order_id="000000000000"></OrderItem>
-            <OrderItem order_id="000000000000"></OrderItem>
-          </thead>
-        </table>
-        <button onClick={handleLogout}>Logout</button>
+  <thead>
+    <tr className="order-header">
+      <td>Order#</td>
+      <td>Quantity</td>
+      <td>Total</td>
+      <td>Status</td>
+      <td>Prepared date</td>
+      <td>Shipped date</td>
+      <td>Delivered date</td>
+    </tr>
+    {orders.map(order => (
+      <OrderItem
+        key={order.order_id}
+        order_id={
+          <Link to={`/order?order_id=${order.order_id}`}>{order.order_id}</Link>
+        }
+        quantity={order.quantity}
+        total={order.sum}
+        status={order.status}
+        prepared_date={order.prepared_date}
+        shipped_date={order.shipping_date}
+        delivered_date={order.delivered_date}
+      />
+    ))}
+  </thead>
+</table>
       </div>
     </div>
   );
