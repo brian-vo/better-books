1ca19dfa09dc48ce714afd7c97fe3f1a019bbdfa
brian-vo <brian.vo1@ucalgary.ca>
2022-12-04 16:35:03 -0700

    add sidenav to handle new account page, add acc page

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -11,7 +11,8 @@ const SideNav = () => {
   
     return (
       <div className="sidenav">
-        <Link to="/account">Order History</Link>
+        <Link to="/account">Update Information</Link>
+        <Link to="/order_history">Order History</Link>
         <Link to="/wishlist">Wishlist</Link>
         <Link to="/recommendations">Recommendations</Link>
         <button onClick={handleLogout} className="logout-button" style={{ position: "absolute", bottom: 0 }}>Logout</button>    </div>
