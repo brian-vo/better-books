3bfed974f0f3db07608db0c5e8acfb1520795032
brian-vo <brian.vo1@ucalgary.ca>
2022-12-07 14:45:37 -0700

    admin update points

--- src/components/SideNavAdmin.js
+++ src/components/SideNavAdmin.js
@@ -3,16 +3,48 @@ import { useNavigate, Link } from 'react-router-dom';
 
 const SideNav = () => {
   const navigate = useNavigate();
+  const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
 
   const handleLogout = () => {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     navigate("/");
   };
 
+  const handleUpdatePoints = async (user_id, points) => {
+    try {
+      const response = await fetch(`/points/${user_id}/update`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          points: points,
+        }),
+      });
+      response.json().then((responseData) => {
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFormLinkClick = (event) => {
+    event.preventDefault();
+
+    const userId = prompt("Enter user ID:");
+    const updatedPoints = prompt("Enter updated points:");
+
+    handleUpdatePoints(userId, updatedPoints);
+    alert("Updated points for user "  + userId);
+  };
+
   return (
     <div className="sidenav">
       <Link to="/admin/orders">All Orders</Link>
       <Link to="/admin/reviews">All Reviews</Link>
+      <Link to="#" onClick={handleFormLinkClick}>Update Points</Link>
       <button onClick={handleLogout} className="logout-button" style={{ position: "absolute", bottom: 0 }}>Logout</button>
     </div>
   );
