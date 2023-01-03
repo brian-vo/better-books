ded839a5e8fe893d7d77e29333fe540a11ec53a4
brian-vo <brian.vo1@ucalgary.ca>
2023-01-03 02:04:28 -0700

    implement star ratings on indvidual reviews

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,9 @@
+import Rating from '@mui/material/Rating';
+
 const Review = ({ review, displayUser = true }) => {
   return (
     <div className="review">
-            {review.rating}/5 - {displayUser && <>User #{review.user_id} - </>}   {review.post_date}
+            <Rating value={review.rating} size="small" readOnly /> - {displayUser && <>User #{review.user_id} - </>}   {review.post_date}
       <br />
       <strong> {review.message_title}</strong>
       <br />
